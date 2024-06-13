6e4a7f98c697e93ba6f59c1b3dac78e6978b1c11
Malik <malik.sayyed415@gmail.com>
2024-06-13 12:28:54 +0530

    deploy

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://staging.d2lxfc1uxunlcr.amplifyapp.com",
+    origin: "https://main.d35u9840ycqal0.amplifyapp.com",
     allowedHeaders: ["Accept", "Content-Type", "Authorization"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     preflightContinue: false,
